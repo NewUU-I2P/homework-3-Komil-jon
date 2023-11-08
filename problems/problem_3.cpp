7b5bc8239119dab5c41ce94d36d7b81485bb7462
komiljon <k.qosimov@newuu.uz>
2023-11-08 22:40:44 +0500

    2nd try

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,26 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+    std::string result;
+    if (S == 'M'){
+        if (height < 1.70){
+            result = "Short";
+        } else if (height < 1.85){
+            result = "Normal";
+        } else{
+            result = "Tall";
+        }
+    } else{
+        if (height < 1.60){
+            result = "Short";
+        } else if (height < 1.75){
+            result = "Normal";
+        } else{
+            result = "Tall";
+        }
 
+    }
     // use return to return your result
+    return result;
     // make use of control flow statements
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,19 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    std::string result;
+    int number;
     // make use of control flow statements
+    number = long(&macAddress[1]) + long(&macAddress[0]) * 16;
+    if (number == 256){
+        result = "Broadcast";
+        return result;
+    }
+    if (number % 2 == 0){
+        result = "Unicast";
+    } else{
+        result = "Multicast";
+    }
     // return result;
+    return result;
 }
